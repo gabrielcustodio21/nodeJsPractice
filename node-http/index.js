77bc0296cc95a1ef405c3a79b35ea6c137df9258
Gabriel Custodio <gkustodor@gmail.com>
2021-05-23 17:29:44 -0300

    Fixed bug on Content-Type.

--- node-http/index.js
+++ node-http/index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req,res) => {
     console.log(req.headers)
 
     res.statusCode = 200;
-    res.setHeader('Content type', 'text/html');
+    res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>Hello, world!</h1></body></html>');
 })
 
