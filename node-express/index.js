094a6d5da814a091274f6cc1a70ccb73a30bb33b
Gabriel Custodio <gkustodor@gmail.com>
2021-05-30 11:37:56 -0300

    Added promoRouter module.

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require ('./routes/dishRouter');
+const promoRouter = require('./promotions/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,6 +14,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 app.use(express.static(__dirname + '/public')); 
 
 
