b0cdc2e649a10bb943cd008c0e48358de6f8470d
Gabriel Custodio <gkustodor@gmail.com>
2021-05-30 11:29:40 -0300

    Added route to /:dishes to the express server

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -28,26 +28,26 @@ dishRouter.route('/')
 
 });
 
+dishRouter.route('/:dishId')
+.get((req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+
+})
+
+.post((req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+})
+
+.put((req,res,next) => {
+    res.write('Updating the dish: ' +req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description); 
+
+})
+
+.delete((req,res,next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+
+});
 
 module.exports = dishRouter;
-
-//app.get('/dishes/:dishId', (req,res,next) => {
-//    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-//
-//});
-
-//app.post('/dishes/:dishId', (req,res,next) => {
-//    res.statusCode = 403;
-//    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-//});
-
-//app.put('/dishes/:dishId', (req,res,next) => {
-//    res.write('Updating the dish: ' +req.params.dishId + '\n');
-//    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description); 
-//
-//});
-
-//app.delete('/dishes/:dishId', (req,res,next) => {
-//    res.end('Deleting dish: ' + req.params.dishId);
-//
-//});
